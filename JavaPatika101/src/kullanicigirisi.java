d82700b98dd76d3ba4d9e32cc0f2ac0f4099d720
alperdevelops <81530776+alperdevelops@users.noreply.github.com>
2022-01-21 02:39:16 +0300

    Patika Java101 kullanıcı Girişi

---
+++ JavaPatika101/src/kullanicigirisi.java
@@ -0,0 +1,59 @@
+import java.security.KeyStore;
+import java.util.Scanner;
+
+public class kullanicigirisi {
+    public static void main(String[] args){
+
+        String userName,password,yeniSifre,cevap;
+        Boolean Evet=true, Hayir=false;
+
+
+
+        Scanner inp = new Scanner(System.in);
+
+
+        System.out.print("Kullanıcı adını giriniz: ");
+        userName = inp.nextLine();
+
+
+        System.out.print("Şifrenizi giriniz: ");
+        password = inp.nextLine();
+
+
+        if (userName.equals("patika") && password.equals("java123")) {
+            System.out.println("Giriş Yaptınız! ");
+        }else {
+            System.out.println("Kullanıcı adı veya Şifreniz hatalı!!! ");
+
+
+            System.out.println("Yeni şifre almak ister misiniz?  (Evet) veya (Hayir) olarak cevaplayınız. ");
+             cevap = inp.nextLine();
+            cevap = (Evet) ? "Yeni şifrenizi giriniz:" : "Programı kapatabilirsiniz.";
+
+            System.out.println("Yeni şifrenizi giriniz: ");
+            yeniSifre = inp.nextLine();
+            if (yeniSifre.equals("java123")){
+                System.out.println("Şifre oluşturulamadı, lütfen başka şifre deneyiniz... ");
+            }else{
+                System.out.println("Yeni şifre oluşturuldu...");
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+        }
+
+
+
+
+    }
+
+
+
